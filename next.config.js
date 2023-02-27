3bc9dfab90632ba3a3f2318137256529b3a89f8f
atosystem <atosystem@hotmail.com>
2023-02-27 17:31:55 +0800

    fix github action

--- next.config.js
+++ next.config.js
@@ -19,19 +19,20 @@ module.exports = {
   assetPrefix: assetPrefix,
   basePath: basePath,
   images: {
+    unoptimized: true
     // loader: 'imgix',
-    domains: [
-            'www.notion.so',
-            'notion.so',
-            'images.unsplash.com',
-            'pbs.twimg.com',
-            'abs.twimg.com',
-            's3.us-west-2.amazonaws.com',
-            'transitivebullsh.it'
-          ],
-    formats: ['image/avif', 'image/webp'],
-    dangerouslyAllowSVG: true,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    // domains: [
+    //         'www.notion.so',
+    //         'notion.so',
+    //         'images.unsplash.com',
+    //         'pbs.twimg.com',
+    //         'abs.twimg.com',
+    //         's3.us-west-2.amazonaws.com',
+    //         'transitivebullsh.it'
+    //       ],
+    // formats: ['image/avif', 'image/webp'],
+    // dangerouslyAllowSVG: true,
+    // contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
 }
 
