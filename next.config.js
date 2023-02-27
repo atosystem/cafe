878b0b3ee7e1745f31fdd833fa88d6b1c9602696
atosystem <atosystem@hotmail.com>
2023-02-27 14:28:42 +0800

    test github action

--- next.config.js
+++ next.config.js
@@ -3,20 +3,53 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
 })
 
-module.exports = withBundleAnalyzer({
-  staticPageGenerationTimeout: 300,
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   images: {
+    loader: 'imgix',
     domains: [
-      'www.notion.so',
-      'notion.so',
-      'images.unsplash.com',
-      'pbs.twimg.com',
-      'abs.twimg.com',
-      's3.us-west-2.amazonaws.com',
-      'transitivebullsh.it'
-    ],
+            'www.notion.so',
+            'notion.so',
+            'images.unsplash.com',
+            'pbs.twimg.com',
+            'abs.twimg.com',
+            's3.us-west-2.amazonaws.com',
+            'transitivebullsh.it'
+          ],
     formats: ['image/avif', 'image/webp'],
     dangerouslyAllowSVG: true,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
-  }
-})
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+}
+
+// module.exports = withBundleAnalyzer({
+//   staticPageGenerationTimeout: 300,
+//   images: {
+//     domains: [
+//       'www.notion.so',
+//       'notion.so',
+//       'images.unsplash.com',
+//       'pbs.twimg.com',
+//       'abs.twimg.com',
+//       's3.us-west-2.amazonaws.com',
+//       'transitivebullsh.it'
+//     ],
+//     formats: ['image/avif', 'image/webp'],
+//     dangerouslyAllowSVG: true,
+//     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+//     loader: 'akamai',
+//   }
+// })
