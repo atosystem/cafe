50b2aaa2d650121a0047720ffa0c544be4f043a2
atosystem <atosystem@hotmail.com>
2023-02-27 22:09:49 +0800

    fix github action

--- next.config.js
+++ next.config.js
@@ -3,54 +3,55 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
 })
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false
+// const isGithubActions = process.env.GITHUB_ACTIONS || false
 
-let assetPrefix = ''
-let basePath = '/'
+// let assetPrefix = ''
+// let basePath = ''
 
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+// if (isGithubActions) {
+//   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
 
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
+//   assetPrefix = `/${repo}/`
+//   basePath = `/${repo}`
+// }
 
-module.exports = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  images: {
-    unoptimized: true
-    // loader: 'imgix',
-    // domains: [
-    //         'www.notion.so',
-    //         'notion.so',
-    //         'images.unsplash.com',
-    //         'pbs.twimg.com',
-    //         'abs.twimg.com',
-    //         's3.us-west-2.amazonaws.com',
-    //         'transitivebullsh.it'
-    //       ],
-    // formats: ['image/avif', 'image/webp'],
-    // dangerouslyAllowSVG: true,
-    // contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-  },
-}
-
-// module.exports = withBundleAnalyzer({
-//   staticPageGenerationTimeout: 300,
+// module.exports = {
+//   assetPrefix: assetPrefix,
+//   basePath: basePath,
 //   images: {
+//     // unoptimized: true
+//     loader: 'imgix',
 //     domains: [
-//       'www.notion.so',
-//       'notion.so',
-//       'images.unsplash.com',
-//       'pbs.twimg.com',
-//       'abs.twimg.com',
-//       's3.us-west-2.amazonaws.com',
-//       'transitivebullsh.it'
-//     ],
+//             'www.notion.so',
+//             'notion.so',
+//             'images.unsplash.com',
+//             'pbs.twimg.com',
+//             'abs.twimg.com',
+//             's3.us-west-2.amazonaws.com',
+//             'transitivebullsh.it'
+//           ],
 //     formats: ['image/avif', 'image/webp'],
 //     dangerouslyAllowSVG: true,
 //     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+//   },
+// }
+
+module.exports = withBundleAnalyzer({
+  staticPageGenerationTimeout: 300,
+  images: {
+    unoptimized: true,
+    domains: [
+      'www.notion.so',
+      'notion.so',
+      'images.unsplash.com',
+      'pbs.twimg.com',
+      'abs.twimg.com',
+      's3.us-west-2.amazonaws.com',
+      'transitivebullsh.it'
+    ],
+    formats: ['image/avif', 'image/webp'],
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
 //     loader: 'akamai',
-//   }
-// })
+  }
+})
